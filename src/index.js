cd4c78fefd27be627e30434c077df209f047895c
kieran <KieranT94@gmail.com>
2019-11-27 16:47:11 -0600

    Further refactor dir structure

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import 'dotenv/config'
 import cors from 'cors'
 import express from 'express'
+import path from 'path'
 
 const app = express()
 
 app.use(cors())
 
+app.use(express.static(__dirname))
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile(path.join(__dirname + '/views/index.html'))
+})
+
+app.get('/cornell', (req, res) => {
+  res.sendFile(path.join(__dirname + '/views/cornell-cs61a-1.html'))
+})
+
+app.get('/ssearch', (req, res) => {
+  res.sendFile(path.join(__dirname + '/views/search-results.html'))
 })
 
 app.listen(3000, () =>
