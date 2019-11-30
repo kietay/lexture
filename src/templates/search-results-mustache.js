532c6794186aa321f3170ad6f823307c4963416d
103033213 <brianslho@gmail.com>
2019-11-29 21:49:07 -0500

    Added search-results page template for mustache.

---
+++ src/templates/search-results-mustache.js
@@ -0,0 +1,74 @@
+var variableData = {
+    searchTerm: "test",
+    resultCount: 20,
+    filterTypes: {
+        lecturers: [
+            {id: 1, name: "Kieran Taylor"},
+            {id: 2, name: "Barack Obama"}
+        ],
+        times: [
+            {id: 1, range: "< 15 min"},
+            {id: 2, range: "15 - 30 min"},
+            {id: 3, range: "30 - 60 min"},
+            {id: 4, range: "> 60 min"}
+        ],
+        transcripts: [
+            {id: 1, language: "English"},
+            {id: 2, language: "中文"},
+            {id: 3, language: "Español"},
+            {id: 4, language: "Binary"}
+        ]
+    }
+    searchResultEntries: [
+        {
+            id: 1,
+            title: "Course Title 1",
+            lecturer: "Professor 1",
+            time: "1h 23m",
+            topics: [
+                {tag: "Algorithms"},
+                {tag: "Introductions"},
+                {tag: "Computer Science"}
+            ],
+            transcriptSearchResults: [
+                // I haven't programmed the mechanic for only the top 2 results to be shown.
+                {
+                    iid: 1,
+                    snippet: "will be on the <span class="search-term-highlight">test</span> for the next quiz next Tuesday.",
+                    timestamp: "00:42"
+                },
+                {
+                    iid: 2,
+                    snippet: "won't be on the <span class="search-term-highlight">test</span>, it's just fun to talk about.",
+                    timestamp: "04:20"
+                }
+            ],
+            url: "http://lexture.com/video1.html"
+        },
+        {
+            id: 2,
+            title: "Course Title 2",
+            lecturer: "Professor 2",
+            time: "2h 34m",
+            topics: [
+                {tag: "Machine Learning"},
+                {tag: "Nearest Neighbors"},
+                {tag: "Computer Science"}
+            ],
+            transcriptSearchResults: [
+                // I haven't programmed the mechanic for only the top 2 results to be shown.
+                {
+                    iid: 1,
+                    snippet: "might be on the <span class="search-term-highlight">test</span> for the next quiz next Tuesday.",
+                    timestamp: "00:24"
+                },
+                {
+                    iid: 2,
+                    snippet: "might not be on the <span class="search-term-highlight">test</span>, it's just fun to talk about.",
+                    timestamp: "02:40"
+                }
+            ],
+            url: "http://lexture.com/video2.html"
+        }
+    ]
+}
--- src/views/templates/video-mustache.js
+++ src/views/templates/video-mustache.js
@@ -1,44 +1,50 @@
+var variableData = {
+    course: "Berkeley CS 61A",
+    title: "Week 1: Functions",
+    textSearchResults:
+        [
+            {
+                textSearchSnippet: "will be on the <span class=\"search-term-highlight\">test</span> for the next quiz next Tuesday.",
+                textSearchTimestamp: "00:42"
+            },
+            {
+                textSearchSnippet: "won't be on the <span class=\"search-term-highlight\">test</span>, it's just fun to talk about.",
+                textSearchTimestamp: "04:20"
+            },
+            {
+                textSearchSnippet: "to study for the <span class=\"search-term-highlight\">test</span> that's coming out this Friday.",
+                textSearchTimestamp: "42:00"
+            }
+        ],
+    videoUrl: "https://lexture.nyc3.digitaloceanspaces.com/sample_lectures/berkeley_cs61a_1.mp4",
+    lecturer: "Kieran Taylor",
+    tags: [
+        {tag: "Algorithms"},
+        {tag: "Computer Science"},
+        {tag: "Hot Lecturer"}
+    ],
+    overview: "Kieran is a proficient speaker of Chinese and the world's leading authority on professional bar fighting.\n" +
+        "He was born in a volcano as it erupted, raised by Shaolin masters, and has the power of three legendary pokemon combined.",
+    languages: [
+        {language: "English"},
+        {language: "中文"},
+        {language: "Español"},
+        {language: "Binary"}
+    ],
+    transcriptText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eget malesuada velit. Donec " +
+        "auctor est sapien, luctus lobortis libero congue eu. Nullam commodo placerat sagittis. Phasellus finibus " +
+        "nisi erat, et dictum sapien molestie id. Cras in bibendum risus. Fusce rhoncus magna mi, tristique ornare " +
+        "ipsum consectetur ut."
+};
+
 $(document).ready(function () {
-    var variableData = {
-        course: "Berkeley CS 61A",
-        title: "Week 1: Functions",
-        textSearchResults:
-            [
-                {
-                    textSearchSnippet: "will be on the <span class=\"search-term-highlight\">test</span> for the next quiz next Tuesday.",
-                    textSearchTimestamp: "00:42"
-                },
-                {
-                    textSearchSnippet: "won't be on the <span class=\"search-term-highlight\">test</span>, it's just fun to talk about.",
-                    textSearchTimestamp: "04:20"
-                },
-                {
-                    textSearchSnippet: "to study for the <span class=\"search-term-highlight\">test</span> that's coming out this Friday.",
-                    textSearchTimestamp: "42:00"
-                }
-            ],
-        videoUrl: "https://lexture.nyc3.digitaloceanspaces.com/sample_lectures/berkeley_cs61a_1.mp4",
-        lecturer: "Kieran Taylor",
-        tags: [
-            {tag: "Algorithms"},
-            {tag: "Computer Science"},
-            {tag: "Hot Lecturer"}
-        ],
-        overview: "Kieran is a proficient speaker of Chinese and the world's leading authority on professional bar fighting.\n" +
-            "He was born in a volcano as it erupted, raised by Shaolin masters, and has the power of three legendary pokemon combined.",
-        languages: [
-            {language: "English"},
-            {language: "中文"},
-            {language: "Español"},
-            {language: "Binary"}
-        ],
-        transcriptText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eget malesuada velit. Donec " +
-            "auctor est sapien, luctus lobortis libero congue eu. Nullam commodo placerat sagittis. Phasellus finibus " +
-            "nisi erat, et dictum sapien molestie id. Cras in bibendum risus. Fusce rhoncus magna mi, tristique ornare " +
-            "ipsum consectetur ut."
-    };
-    var title = $(html).filter('title').text();
+    var title = $(html).filter('title').html();
     Mustache.parse(title); // optional, speeds up future uses
     var renderedTitle = Mustache.render(title, variableData);
-    $('#target').html(rendered);
+    $(html).filter('title').html(renderedTitle);
+
+    var breadcrumb = $(".breadcrumb-bar").html();
+    Mustache.parse(breadcrumb);
+    var renderedBreadcrumb = Mustache.render(breadcrumb, variableData);
+    $(".breadcrumb-bar").html(renderedBreadcrumb);
 });
