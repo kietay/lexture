76c392d8c249b1f6d531a1f51c23a83925d8d16c
kieran <KieranT94@gmail.com>
2019-12-03 11:27:08 -0500

    Avoid race condition on video id assignment

--- src/api/video.js
+++ src/api/video.js
@@ -1,6 +1,7 @@
 import express from 'express'
 const router = express.Router()
 import Video from '../models/Video'
+import Caption from '../models/Video'
 import { fetchCourseFromVideoId, fetchVideoMatchingTranscripts, transcriptToSnippet, secondsToTimeString, secondsToColonTime } from './search'
 import {downloadTranscript} from '../services/spaces'
 
@@ -14,6 +15,7 @@ router.get('/', async (req, res) => {
   }
 
   const course = await fetchCourseFromVideoId(vid.videoId)
+  const transcript = downloadTranscript(vid.captions[0].url)
 
   const tags = vid.topics.map(x => ({ tag: x }))
 
@@ -31,6 +33,14 @@ router.get('/', async (req, res) => {
   
   console.log("snippets:")
 
+  const subs = [
+    {
+      lang: 'en',
+      label: 'English',
+      url: "https://lexture.nyc3.digitaloceanspaces.com/" + vid.captions[0].url
+    }
+  ]
+
   const data = {
     course: course.title,
     title: vid.title,
@@ -39,11 +49,16 @@ router.get('/', async (req, res) => {
     tags: tags,
     languages: languages,
     textSearchResults: snippets,
+    subtitleTracks: subs
   }
 
   res.render('video', data)
 })
 
-// downloadTranscript("content/exampleCourse/transcripts/f8087006-ba4b-457e-90c4-1c1b1f473487.vtt").then(data => console.log(data))
+const fetchTranscript = async videoId => {
+  const captions = Caption.find({videoId: videoId}).sort({})
+}
+
+// downloadTranscript("content/exampleCourse/transcripts/6633efce-5e46-416a-ac02-e155d3f65728.vtt").then(data => console.log(data))
 
 export default router
